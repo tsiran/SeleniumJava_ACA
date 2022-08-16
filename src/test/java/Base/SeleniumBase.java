1b0a1e3350ee9006f358382f20d0bab367ad1340
tsiran <107708050+tsiran@users.noreply.github.com>
2022-08-16 21:12:06 +0400

    1 commit

--- src/test/java/Base/SeleniumBase.java
+++ src/test/java/Base/SeleniumBase.java
@@ -2,33 +2,22 @@ package Base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 
 public class SeleniumBase {
-
-    public WebDriver driver;
-
-    /**
-     * This is the main method to init the WebDriver
-     */
-    @BeforeTest
-    public void InitDriver() {
+   public WebDriver driver;
+   @BeforeTest
+    public void InitDriver(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
         driver.manage().window().maximize();
     }
-
-    /**
-     * This is the main method to quit the WebDriver
-     */
     @AfterTest
-    public void quitDriver() {
+    public void quitDriver(){
         driver.quit();
     }
-
 }
--- src/test/java/Page/BasePage.java
+++ src/test/java/Page/BasePage.java
@@ -1,24 +1,14 @@
 package Page;
 
-import org.apache.commons.lang3.ObjectUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-
 
 public class BasePage {
-
     WebDriver driver;
 
     public BasePage(WebDriver driver) {
@@ -27,147 +17,48 @@ public class BasePage {
 
     /**
      * This method returns the Selenium By type.
+     *
      * @param locatorType
      * @param locator
      * @return
      */
-    public By getByType(String locatorType, String locator){
-        if (locatorType.toLowerCase(Locale.ROOT).equals("css")){
+    public By getByType(String locatorType, String locator) {
+        if (locatorType.toLowerCase(Locale.ROOT).equals("css")) {
             return By.cssSelector(locator);
         } else if (locatorType.toLowerCase(Locale.ROOT).equals("xpath")) {
             return By.xpath(locator);
-        } else if (locatorType.toLowerCase(Locale.ROOT).equals("id")){
+        } else if (locatorType.toLowerCase(Locale.ROOT).equals("id")) {
             return By.id(locator);
-        } else if (locatorType.toLowerCase(Locale.ROOT).equals("name")){
+        } else if (locatorType.toLowerCase(Locale.ROOT).equals("name")) {
             return By.name(locator);
         } else {
             return By.tagName("");
         }
     }
 
-    public WebElement getElement(String locatorType, String locator){
+    public WebElement getElement(String locatorType, String locator) {
         By byType = getByType(locatorType, locator);
         WebElement elem = driver.findElement(byType);
         return elem;
     }
 
-    public List<WebElement> getElements(String locatorType, String locator){
-        By byType = getByType(locatorType, locator);
-        List<WebElement> elems = driver.findElements(byType);
-        return elems;
-    }
-
-    public void clickOnElement(String locatorType, String locator){
+    public void clickOnElement(String locatorType, String locator) {
         getElement(locatorType, locator).click();
     }
 
-    public void insertInto(String locatorType, String locator, String text){
+    public void filledTextField(String locatorType, String locator, String inputText) {
         WebElement elem = getElement(locatorType, locator);
         elem.clear();
-        elem.sendKeys(text);
+        elem.sendKeys(inputText);
     }
 
-    /**
-     * Use this method to check the web page title
-     *
-     * @param expectedTitle
-     */
-    public void checkPageTitle(String expectedTitle) {
-        Assert.assertTrue(driver.getTitle().equals(expectedTitle));
+    public void getUrl(String url) {
+        driver.get(url);
     }
 
-
-    /**
-     * The method to implement customs wait
-     *
-     * @param locator
-     * @return
-     */
-    public boolean customTry(String locatorType, String locator) {
-        boolean result = false;
-        int attempts = 0;
-        while (attempts < 5) {
-            try {
-                getElement(locatorType, locator).click();
-                result = true;
-                break;
-            } catch (Exception e) {
-            }
-            attempts++;
-        }
-        return result;
-    }
-
-    /**
-     * Use this method to go to web page menu item by locator and text
-     *
-     * @param itemText
-     * @param locator
-     */
-    public void goToMenuItem(String itemText, String locatorType, String locator) {
-        if (customTry(locatorType, locator)) {
-            List<WebElement> items = getElements(locatorType, locator);
-            boolean isItemExist = false;
-            for (WebElement item : items) {
-                if (item.getText().contains(itemText)) {
-                    item.click();
-                    isItemExist = true;
-                    break;
-                }
-            }
-            Assert.assertTrue(isItemExist);
-        }
-    }
-
-    public void verifyMenuItemText(String itemText, String locatorType, String locator) {
-        if (customTry(locatorType, locator)) {
-            List<WebElement> items = getElements(locatorType, locator);
-            boolean isItemExist = false;
-            for (WebElement item : items) {
-                if (item.getText().equals(itemText)) {
-                    isItemExist = true;
-                    Assert.assertTrue(isItemExist);
-                    break;
-                }
-            }
-            Assert.assertTrue(isItemExist);
-        }
-    }
-
-    /**
-     * Use this method to set delay by seconds between steps.
-     * @param seconds
-     * @throws InterruptedException
-     */
-    public void delay(int seconds) throws InterruptedException {
-        TimeUnit.SECONDS.sleep(seconds);
-    }
-
-    /**
-     * Use this method to set explicitly wait.
-     * @param conditions
-     */
-    public void explicitlyWait(Function conditions){
-        new WebDriverWait(driver, Duration.ofSeconds(5)).until(conditions);
-    }
-
-    public void checkAlertMessage(String message) throws InterruptedException{
-        delay(1);
-        String actualmessage = driver.switchTo().alert().getText();
-        Assert.assertEquals(actualmessage, message, "The Registration Alert message not match");
-    }
-
-    public void alertAccept(){
-        driver.switchTo().alert().accept();
-    }
-
-    public void getUrl(String URL){
-        driver.get(URL);
-    }
-
-    public void checkElemetText(String locatorType, String locator, String expectedText){
-        WebElement elem = getElement(locatorType, locator);
-        String actualText = elem.getText();
-        Assert.assertEquals(actualText, expectedText, "The expected text from element not mutch");
+    public void assertString(String actualString, String expectedString) {
+        Assert.assertEquals(actualString, expectedString);
     }
 }
+
+
